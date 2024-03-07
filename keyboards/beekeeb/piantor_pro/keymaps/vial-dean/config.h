c83f970ec1bc24355435959b5f46ddc234593045
deeaann <glueckler@gmail.com>
2024-03-07 10:42:58 -0700

    dean here, wrote my piantor config

---
+++ keyboards/beekeeb/piantor_pro/keymaps/vial-dean/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0x66, 0xA6, 0x8B, 0x3C, 0x4A, 0x03, 0x93, 0x39}
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+// disable double tap repeat
+#define QUICK_TAP_TERM 0
---
+++ keyboards/beekeeb/piantor_pro_dean/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* keyboard uid */
+#define VIAL_KEYBOARD_UID {0x66, 0xA6, 0x8B, 0x3C, 0x4A, 0x03, 0x93, 0x39}
+
+#define WEAR_LEVELING_LOGICAL_SIZE (8 * 1024)
+#define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)
+
+/* default layer count */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+// disable double tap repeat
+#define QUICK_TAP_TERM 0
